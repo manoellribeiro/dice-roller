cfd4bb12a45812aea677a4216d7bd264dc8e545f
Manoel Ribeiro <mano12328@hotmail.com>
2020-04-11 18:18:21 -0300

    Creating listview with all the dice results and showing a toast if the user gets the same number twice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.diceroller
 
+import android.content.Context
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage: ImageView
+    lateinit var resultListView: ListView
+    lateinit var customAdapter: CustomAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
@@ -20,10 +27,27 @@ class MainActivity : AppCompatActivity() {
             rollDice()
         }
         diceImage = findViewById(R.id.dice_image)
+        resultListView = findViewById(R.id.results_list)
+        customAdapter = CustomAdapter(this, lastResultsImage)
+        resultListView.adapter = customAdapter
     }
 
-    private fun rollDice() {
+    class CustomAdapter(context: Context, private var values: List<Int>) :
+            ArrayAdapter<Int>(context, R.layout.last_result_list_item, values){
 
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val rowView: LinearLayout = LayoutInflater.from(context).inflate(
+                R.layout.last_result_list_item,
+                parent,
+                false) as LinearLayout
+            rowView.findViewById<ImageView>(R.id.last_result_item).setImageResource(values[position])
+            rowView.findViewById<TextView>(R.id.item_number).text = (values.size - position).toString()
+            return rowView
+        }
+    }
+
+    var lastResultsImage: ArrayList<Int> = ArrayList<Int>()
+    private fun rollDice() {
         val randomDiceValue = Random().nextInt(6) + 1
         val diceDrawableResource = when (randomDiceValue){
             1 -> R.drawable.dice_1
@@ -34,5 +58,14 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
         diceImage.setImageResource(diceDrawableResource)
+        lastResultsImage.add(0, diceDrawableResource)
+        customAdapter.notifyDataSetChanged()
+        if (lastResultsImage.size > 1){
+            if (lastResultsImage[0] == lastResultsImage[1]){
+                val toast = Toast.makeText(applicationContext, "You got twice in a roll", Toast.LENGTH_SHORT)
+                toast.setGravity(Gravity.TOP, 160, 200)
+                toast.show()
+            }
+        }
     }
 }
